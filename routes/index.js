21cfdb5227ae9b3b2bbb3fcdc4cdab3dccc752eb
Mark Cheli <mpcheli7@gmail.com>
2017-04-09 19:27:27 -0400

    Added jade templates, updated routes, added success page

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
+var path = require('path');
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 //Subscribe to mailing list
 //TODO: Don't add if already on list
 router.post('/subscribe', function (req, res, next) {
     User.create(req.body);
-    res.send("Success");
+    res.sendFile(path.join(__dirname, '../public/success.html'));
 });
 
 //Unsubscribe from mailing list
 router.post('/unsubscribe', function (req, res, next) {
     User.find(req.body).remove().exec();
-    res.send("Success");
+    res.sendFile(path.join(__dirname, '../public/success.html'));
 });
 
 module.exports = router;
